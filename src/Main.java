2a0bd698a684d494750c9a0cf584536bfb2d3a36
Paul Kristopher Espina <kristopherespina@gmail.com>
2021-03-10 22:04:45 +0800

    now runs up until login

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
+import java.sql.*;
+
 public class Main
 {
     public boolean loggedIn;
 
     public static void main(String[] args)
     {
+        DatabaseConnection dbCon = DatabaseConnection.getInstance();
+        dbCon.init();
         new Login();
     }
 }
