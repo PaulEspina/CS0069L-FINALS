c96af833b82843cf8bb03cefacf1159ed47a716e
Paul Kristopher Espina <kristopherespina@gmail.com>
2021-03-11 18:19:33 +0800

    implemented the JFrame here instead of different JFrames for subclasses

--- src/User.java
+++ src/User.java
@@ -1,21 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+
 public abstract class User
 {
-    protected int userID; // 5 digits xyyyy, x=usertype, y=id
-    protected String username;
-    protected String firstName;
-    protected String middleName;
-    protected String lastName;
+    private int userID; // 5 digits xyyyy, x=usertype, y=id
+    private String username;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    protected JFrame window;
+    protected JPanel headerPanel;
+    protected JPanel navPanel;
+    protected JPanel contentPanel;
 
-    public User()
-    {
-        userID = 0;
-        username = "";
-        firstName = "";
-        middleName = "";
-        lastName = "";
-    }
-
-    public User(int userID, String username, String firstName, String middleName, String lastName)
+    protected User(int userID, String username, String firstName, String middleName, String lastName)
     {
         this.userID = userID;
         this.username = username;
@@ -24,51 +22,96 @@ public abstract class User
         this.lastName = lastName;
     }
 
-    public abstract void logout();
+    public void start()
+    {
+        window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setSize(800, 600);
+        window.setMinimumSize(new Dimension(800, 600));
+//        window.setResizable(false);
+        window.setLocationRelativeTo(null);
+        window.setTitle("Apartment Management System");
+        header();
+        nav();
+        content();
+        window.add(headerPanel, BorderLayout.NORTH);
+        window.add(navPanel, BorderLayout.WEST);
+        window.add(contentPanel, BorderLayout.CENTER);
+        window.setVisible(true);
+    }
+
+    private void header()
+    {
+        headerPanel = new JPanel();
+        headerPanel.setBorder(BorderFactory.createRaisedSoftBevelBorder());
+        headerPanel.setPreferredSize(new Dimension(window.getWidth(), window.getHeight() / 8));
+        headerPanel.setLayout(new BorderLayout());
+
+        JLabel title = new JLabel("Apartment Management System");
+        title.setFont(new Font("Arial", Font.BOLD, 24));
+
+        JPanel userInfoPanel = new JPanel();
+        userInfoPanel.setPreferredSize(new Dimension(window.getWidth() / 6, window.getHeight()));
+        userInfoPanel.setBackground(Color.BLACK);
+        userInfoPanel.setOpaque(true);
+
+        headerPanel.add(title, BorderLayout.CENTER);
+        headerPanel.add(userInfoPanel, BorderLayout.EAST);
+    }
+
+    protected abstract void nav();
+
+    protected abstract void content();
+
+    private void logout()
+    {
+        DatabaseConnection dbCon = DatabaseConnection.getInstance();
+        dbCon.close();
+    }
 
     // Setters
-    public void setUserID(int value)
+    protected void setUserID(int value)
     {
         userID = value;
     }
 
-    public void setUsername(String username)
+    protected void setUsername(String username)
     {
         this.username = username;
     }
 
-    public void setFirstName(String firstName)
+    protected void setFirstName(String firstName)
     {
         this.firstName = firstName;
     }
 
-    public void setMiddleName(String middleName)
+    protected void setMiddleName(String middleName)
     {
         this.middleName = middleName;
     }
 
-    public void setLastName(String lastName)
+    protected void setLastName(String lastName)
     {
         this.lastName = lastName;
     }
 
     // Getters
-    public int getUserID()
+    protected int getUserID()
     {
         return userID;
     }
 
-    public String getFirstName()
+    protected String getFirstName()
     {
         return firstName;
     }
 
-    public String getMiddleName()
+    protected String getMiddleName()
     {
         return middleName;
     }
 
-    public String getLastName()
+    protected String getLastName()
 
     {
         return lastName;
