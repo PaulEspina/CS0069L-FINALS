de0273a347696c91de13bb32e67467babb393a3a
hirara20701 <hyper15francismiguel@gmail.com>
2021-03-22 09:20:37 +0800

    Added Table (I don't know if this is correct)

--- src/TenantTest.java
+++ src/TenantTest.java
@@ -4,6 +4,13 @@ public class TenantTest {
         DatabaseConnection dbCon = DatabaseConnection.getInstance();
         dbCon.init();
 
-        new TenantController(new Tenant(100000, "default_tenant", "", "", "", "default_pic.png"));
+        //new TenantController(new Tenant(100000, "default_tenant", "", "", "", "default_pic.png"));
+//        dbCon.execute("INSERT INTO users VALUES('200000', 'tenant', 'tenant', 'Paul Kristopher', 'D', 'Espina', 'default_pic.png')");
+//
+//        dbCon.execute("INSERT INTO bills('recipient_id', 'date_issued', 'total_amount', 'amount_paid') VALUES('200000', '01/05/2021', '3000', '0') ");
+//        dbCon.execute("INSERT INTO bills('recipient_id', 'date_issued', 'total_amount', 'amount_paid') VALUES('200000', '02/05/2021', '3000', '0') ");
+//        dbCon.execute("INSERT INTO bills('recipient_id', 'date_issued', 'total_amount', 'amount_paid') VALUES('200000', '03/05/2021', '3000', '0') ");
+//        dbCon.execute("INSERT INTO bills('recipient_id', 'date_issued', 'total_amount', 'amount_paid') VALUES('200000', '04/05/2021', '3000', '0') ");
+//        dbCon.execute("INSERT INTO bills('recipient_id', 'date_issued', 'total_amount', 'amount_paid') VALUES('200000', '05/05/2021', '3000', '0') ");
     }
 }
