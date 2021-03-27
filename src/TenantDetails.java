3ff0ea7791760961f5cee5a876a5e31a171fbad2
Paul Kristopher Espina <kristopherespina@gmail.com>
2021-03-28 00:03:12 +0800

    now delete's tenant's image data when a tenant is removed.

--- src/TenantDetails.java
+++ src/TenantDetails.java
@@ -1,11 +1,10 @@
-import jdk.nashorn.internal.scripts.JO;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import java.io.File;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -281,8 +280,12 @@ public class TenantDetails extends JFrame implements ActionListener, WindowListe
             int x = JOptionPane.showConfirmDialog(null, "Are you sure you want to remove this tenant?", "Confirm Remove" , JOptionPane.YES_NO_OPTION);
             if(x == JOptionPane.YES_OPTION)
             {
+                File file = new File("image/" + tenantID);
+                //noinspection ResultOfMethodCallIgnored
+                file.delete();
                 connection.execute("DELETE FROM users WHERE key='" + tenantID + "'");
                 connection.execute("DELETE FROM tenants WHERE key='" + tenantID + "'");
+                dispose();
             }
         }
 
