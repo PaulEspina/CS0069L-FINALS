d3ea310f9af357558f5802f0199cc70233ac175f
Paul Kristopher Espina <kristopherespina@gmail.com>
2021-03-28 00:25:05 +0800

    refactored

--- src/ChangePassword.java
+++ src/ChangePassword.java
@@ -2,25 +2,23 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class ChangePassword extends JFrame implements ActionListener
 {
-    private DatabaseConnection connection;
     private final User user;
 
     private final JButton confirm = new JButton();
-    private final JPasswordField currentPassword = new JPasswordField();
+    private final JPasswordField currentPasswordField;
+    private final JPasswordField newPasswordField;
+    private final JPasswordField confirmPasswordField;
 
     ChangePassword(User user)
     {
         this.user = user;
 
-        //Francis - Frame Settings
-        setVisible(true);
+        // Frame Settings
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setResizable(false);
         setSize(400, 200);
@@ -28,60 +26,51 @@ public class ChangePassword extends JFrame implements ActionListener
         setLocationRelativeTo(null);
         setTitle("Change Password");
 
-        //Francis - Current Password Message
-        JLabel currentPass = new JLabel();
-        currentPass.setText("Please enter current password: ");
-        currentPass.setFont(new Font("Courier", Font.BOLD, 12));
-        currentPass.setLayout(null);
-        currentPass.setBounds(15, 20, 250, 25);
+        // Current Password Message
+        JLabel currentPassLabel = new JLabel();
+        currentPassLabel.setText("Enter current password: ");
+        currentPassLabel.setBounds(25, 20, 150, 25);
 
-        //Francis - New Password Message
-        JLabel newPass = new JLabel();
-        newPass.setText("Please enter new password: ");
-        newPass.setFont(new Font("Courier", Font.BOLD, 12));
-        newPass.setLayout(null);
-        newPass.setBounds(15, 50, 250, 25);
+        // New Password Message
+        JLabel newPassLabel = new JLabel();
+        newPassLabel.setText("Enter new password: ");
+        newPassLabel.setBounds(25, 50, 150, 25);
 
-        //Francis - Confirm Password Message
-        JLabel confirmPass = new JLabel();
-        confirmPass.setText("Please confirm your password: ");
-        confirmPass.setFont(new Font("Courier", Font.BOLD, 12));
-        confirmPass.setLayout(null);
-        confirmPass.setBounds(15, 80, 250, 25);
+        // Confirm Password Message
+        JLabel confirmPassLabel = new JLabel();
+        confirmPassLabel.setText("Confirm new password: ");
+        confirmPassLabel.setBounds(25, 80, 150, 25);
 
-        //Francis - Textfield for Current Password
-        currentPassword.setBounds(200,20,180,25);
-        currentPassword.setVisible(true);
+        // Textfield for Current Password
+        currentPasswordField = new JPasswordField();
+        currentPasswordField.setBounds(175, 20, 175, 25);
 
-        //Francis - Textfield for New Password
-        JPasswordField newPassword = new JPasswordField();
-        newPassword.setBounds(200, 50, 180, 25);
-        newPassword.setVisible(true);
+        // Textfield for New Password
+        newPasswordField = new JPasswordField();
+        newPasswordField.setBounds(175, 50, 175, 25);
 
-        //Francis - Textfield for Confirm Password
-        JPasswordField confirmPassword = new JPasswordField();
-        confirmPassword.setBounds(200, 80, 180, 25);
-        confirmPassword.setVisible(true);
+        // Textfield for Confirm Password
+        confirmPasswordField = new JPasswordField();
+        confirmPasswordField.setBounds(175, 80, 175, 25);
 
-        //Francis - Confirm Button
+        // Confirm Button
         confirm.setText("Confirm");
-        confirm.setBackground(Color.GRAY);
+        confirm.setBackground(Color.WHITE);
         confirm.setBounds(290,130, 80,25);
-        confirm.setFont(new Font("Courier", Font.PLAIN,10));
+        confirm.setFont(new Font("Arial", Font.PLAIN,10));
         confirm.setFocusable(false);
         confirm.setOpaque(true);
-        confirm.setVisible(true);
         confirm.addActionListener(this);
 
-        //Francis - add to the frame
-        add(currentPass);
-        add(newPass);
-        add(confirmPass);
-        add(currentPassword);
-        add(newPassword);
-        add(confirmPassword);
+        add(currentPassLabel);
+        add(newPassLabel);
+        add(confirmPassLabel);
+        add(currentPasswordField);
+        add(newPasswordField);
+        add(confirmPasswordField);
         add(confirm);
 
+        setVisible(true);
     }
 
     //Francis - All actions are here
@@ -90,33 +79,43 @@ public class ChangePassword extends JFrame implements ActionListener
     {
         if(e.getSource() == confirm)
         {
-            String password = null;
-            connection = DatabaseConnection.getInstance();
-            ResultSet rs = connection.getResult("SELECT password FROM users WHERE key ='" + user.getUserID()+ "'");
-            try
+            if(!String.valueOf(currentPasswordField.getPassword()).isEmpty() &&
+               !String.valueOf(newPasswordField.getPassword()).isEmpty() &&
+               !String.valueOf(confirmPasswordField.getPassword()).isEmpty())
             {
-                password = rs.getString("password");
-            }
-            catch (SQLException throwables)
-            {
-                throwables.printStackTrace();
-            }
-            if(String.valueOf(currentPassword.getPassword()).equals(password))
-            {
-                if(String.valueOf(currentPassword.getPassword()).equals(String.valueOf(currentPassword.getPassword())))
+                String password = "";
+                DatabaseConnection connection = DatabaseConnection.getInstance();
+                ResultSet resultSet = connection.getResult("SELECT password FROM users WHERE key ='" + user.getUserID() + "'");
+                try
                 {
-                    connection.execute("UPDATE users SET password='" + String.valueOf(currentPassword.getPassword()) + "' WHERE key ='" + user.getUserID()+ "'");
-                    JOptionPane.showMessageDialog(null,"Password Updated!","Congratulations", JOptionPane.WARNING_MESSAGE);
-                    dispose();
+                    password = resultSet.getString("password");
+                    resultSet.close();
+                }
+                catch(SQLException throwables)
+                {
+                    throwables.printStackTrace();
+                }
+                if(String.valueOf(currentPasswordField.getPassword()).equals(password))
+                {
+                    if(String.valueOf(newPasswordField.getPassword()).equals(String.valueOf(confirmPasswordField.getPassword())))
+                    {
+                        connection.execute("UPDATE users SET password='" + String.valueOf(currentPasswordField.getPassword()) + "' WHERE key ='" + user.getUserID() + "'");
+                        JOptionPane.showMessageDialog(null, "Password Updated!", "Congratulations", JOptionPane.INFORMATION_MESSAGE);
+                        dispose();
+                    }
+                    else
+                    {
+                        JOptionPane.showMessageDialog(null, "Password did not match!", "Password Mismatch", JOptionPane.ERROR_MESSAGE);
+                    }
                 }
                 else
                 {
-                    JOptionPane.showMessageDialog(null,"Password Did Not Match!","ERROR",JOptionPane.WARNING_MESSAGE);
+                    JOptionPane.showMessageDialog(null, "Current password entered, did not match!", "Wrong Password", JOptionPane.ERROR_MESSAGE);
                 }
             }
             else
             {
-                JOptionPane.showMessageDialog(null,"Wrong Password!","ERROR",JOptionPane.WARNING_MESSAGE);
+                JOptionPane.showMessageDialog(null, "Please fill all of the fields.", "Insuffiecient Data", JOptionPane.WARNING_MESSAGE);
             }
         }
     }
